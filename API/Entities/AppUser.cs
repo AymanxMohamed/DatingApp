0ecd00af844d1f9a2ee5c84429b99ea5baf951fd
Ayman Kheireldeen <aymanmido067@gmail.com>
2022-03-27 12:25:28 +0200

    Finishing User Controller api

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        // use the name id to make entity frame work automaticly recognize the id field and it will make it primary key
+        // you have to use the naming convention properties is pascal case
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# DatingApp
+
+## Entity Frame Work
+
+> it's an Object Relational Mapper (ORM)
+> Translates our code into SQL commands that update our tables in the database
