3ff7f3e113044cab1bdc5ae3cf1516568aa179ce
Ayman Kheireldeen <aymanmido067@gmail.com>
2022-03-27 13:24:23 +0200

    getting the users from the api to the client app

--- API/Startup.cs
+++ API/Startup.cs
@@ -45,6 +45,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+            // This service helps you to enable cors in the api
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +62,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
